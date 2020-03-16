a950affaed39ae884afeeaabe6c92f7be80a625d
Samantha Emily-Rachel Belnavis <samanthabelnavis@gmail.com>
2020-03-15 22:15:57 -0400

    working discord connection
    originally attempted to use discord bot, but ended up going with webhooks because the API had changed so much. app uses a pseudo-bot user to give the appearance as if it’s a bot to lessen the confusion with users on the discord server.
    
    Signed-off-by: Samantha Emily-Rachel Belnavis <samanthabelnavis@gmail.com>

--- index.js
+++ index.js
@@ -1,46 +1,60 @@
-const { BitlyClient } = require('bitly');
-const firebase = require('firebase');
-const get = require('get-value');
-const http = require('http');
-const moment = require('moment');
-const { Probot } = require('probot');
+const { Client } = require('discord.js')
+const get = require('get-value')
+const http = require('http')
+const hookcord = require('hookcord')
+const moment = require('moment')
+const { Probot } = require('probot')
 
-const configFirebase = {
-  apiKey: process.env.FIREBASE_API_KEY,
-  databaseURL: process.env.FIREBASE_DATABASE_URL
-}
+const botUser = new Client()
+botUser.login(process.env.DISCORD_BOT_TOKEN)
 
-const bitly = new BitlyClient(process.env.BITLY_API_KEY, {})
+const releaseHook = new hookcord.Hook()
+releaseHook.login(process.env.DISCORD_ID, process.env.DISCORD_SECRET)
 
-firebase.initializeApp(configFirebase);
-
-
+const devHook = new hookcord.Hook()
+devHook.login(process.env.DISCORD_DEV_WEBHOOK_ID, process.env.DISCORD_DEV_WEBHOOK_SECRET)
 module.exports = release => {
-  release.on(['release'], async context => {
+
+  release.on('release', async context => {
     const id = get(context, 'id')
     const repo = get(context.payload, 'repository.name')
     const org = get(context.payload, 'organization.login')
     const author = get(context.payload, 'release.author.login')
     const version = get(context.payload, 'release.tag_name')
-    const releaseDate = get(context.payload, 'release.published_at').split("T")[0]
     const link = `https://github.com/${org}/${repo}/releases/tag/${version}`
 
-    firebase.database().ref('releases/' + repo + "/latest" ).set({
-      author: author,
-      version: version,
-      release_date: releaseDate,
-      link: link
+    const msg = `A newer version of ${repo} (${version}) has been released. You can view it here: ${link}`
+    releaseHook.setPayload({
+      "content": msg
     })
 
-    firebase.database().ref('releases/' + repo + "/" + id).set({
-      author: author,
-      version: version,
-      release_date: releaseDate,
-      link: link
+    releaseHook.fire().then(response_object => {
+
+    }).catch(error => {
+      throw error
     })
   })
 }
 
 module.exports = pullRequest => {
+  pullRequest.on('pull_request', async context => {
+    const id = get(context, 'id')
+    const repo = get(context.payload)
+    const org = get(context.payload, 'organization.login')
+    const num = get(context.payload, 'number')
+    const state = get(context.payload, 'action')
+    const link = `https://github.com/${org}/${repo}/pull/${num}`
 
+    let msg
+    if (state === 'opened') {
+      msg = `A pull request (#${num}) for ${repo} was opened. You can view it here: ${link}`
+    } else if (state === 'closed') {
+      msg = `A pull request (#${num}) for ${repo} was closed. You can view it here: ${link}`
+    } else if (state === 'synchronize') {
+      msg = `A pull request (#${num}) for ${repo} was updated. You can view it here: ${link}`
+    }
+
+    devHook.setPayload({ "content": msg})
+    devHook.fire().then().catch(error => { throw error})
+  })
 }
